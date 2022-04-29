8430ce54f04c83d382cdae0a18ca2fd621f786c1
lethanhtrung7412 <lethanhtrung7412@gmail.com>
2022-04-29 18:23:38 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import Popup from "./Popup";
+
+const headers = {
+  "Content-Type": "application/json",
+  apikey: "036d2025789d4ca496a6564a87b0c36d",
+};
 
 function App() {
+  const [text, setText] = React.useState("");
+  const [returnUrl, setReturnUrl] = React.useState("");
+  const [isShowResult, setIsShowResult] = React.useState(false);
+
+  async function shorten(url) {
+    let endpoint = "https://api.rebrandly.com/v1/links";
+    let linkRequest = {
+      destination: url,
+      domain: { fullName: "rebrand.ly" },
+    };
+    const apiCall = {
+      method: "post",
+      url: endpoint,
+      data: linkRequest,
+      headers: headers,
+    };
+    let apiResponse = await axios(apiCall);
+    let link = apiResponse.data;
+    setReturnUrl(link.shortUrl);
+  }
+
+  function handleChange(e) {
+    setText(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    shorten(text);
+  }
+
+  function handlePopup() {
+    setIsShowResult((isShowResult) => !isShowResult);
+  }
+
+  function closePopup() {
+    setIsShowResult((isShowResult) => !isShowResult);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Want to shorten your link, you come to the right place</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <h3 style={{ margin: "auto" }}>Type in your links</h3>
+          <br />
+          <input
+            type="text"
+            className="input"
+            value={text}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <input
+          type="submit"
+          value="Get your link"
+          onClick={handlePopup}
+          className="btn"
+        />
+      </form>
+      <Popup
+        isShowResult={isShowResult}
+        returnUrl={returnUrl}
+        handleClick={closePopup}
+      />
     </div>
   );
 }
---
+++ src/Popup.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Popup.css";
+
+const Popup = (props) => {
+  return (
+    <div className={`${props.isShowResult ? "active" : ""} show`}>
+      <div className="form-box">
+        <h2>Your link after shorten</h2>
+        <h3>{props.returnUrl}</h3>
+        <div id="modal" onClick={props.handleClick} className="btn-close">
+          Close
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
